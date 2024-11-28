d9e83399ef34a62605dc1ad856fa952b2b39d944
Kelompok 13 Deep Learning - Sains Data 2024/2025 <deeplearning13project@gmail.com>
2024-11-29 01:39:45 +0700

    Update website.py

--- website.py
+++ website.py
@@ -101,9 +101,8 @@ html_code = """
     </div>
     <div class="footer">
         <h4>© Developer: Kelompok 13 Deep Learning</h4>
-        <a href="https://github.com/sains-data/Model-Transfer-Learning-untuk-Klasifikasi-Suara-Burung-di-TNWK">
-          <img src="https://raw.githubusercontent.com/alberanalafean22/DeteksiKlasifikasiSpesiesBurung/main/Deployment/asset/logogithub.png" alt="Github">
-        </a>
+        <img src="https://raw.githubusercontent.com/alberanalafean22/DeteksiKlasifikasiSpesiesBurung/main/Deployment/asset/logogithub.png" width="20" height="20" alt="Logo GitHub">
+        <span>https://github.com/sains-data/Model-Transfer-Learning-untuk-Klasifikasi-Suara-Burung-di-TNWK</span>
     </div>
 </body>
 </html>
