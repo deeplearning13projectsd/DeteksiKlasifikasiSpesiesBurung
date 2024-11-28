0483d54faba6cdee711841f6d5effbc4e5d0f617
Kelompok 13 Deep Learning - Sains Data 2024/2025 <deeplearning13project@gmail.com>
2024-11-29 03:29:33 +0700

    Update website.py

--- website.py
+++ website.py
@@ -137,5 +137,5 @@ if uploaded_file:
     image_url = get_image_url(file_name)
     
     # Tampilkan gambar hasil klasifikasi
-    st.image(image_url, caption=f"Hasil klasifikasi untuk {file_name}", use_column_width=True)
+    st.image(image_url, caption=f"Hasil klasifikasi untuk {file_name}", use_column_width=False)
 
