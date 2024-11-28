3308664fe7a30d8797c1764915ba8b1626618527
Kelompok 13 Deep Learning - Sains Data 2024/2025 <deeplearning13project@gmail.com>
2024-11-29 01:52:35 +0700

    Update test.py

--- test.py
+++ test.py
@@ -40,7 +40,7 @@ def create_html_content(language):
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
       <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet">
       <style>
-                 body {
+            body {
             background-size: cover;
             color: white;
             font-family: 'Roboto', sans-serif;
