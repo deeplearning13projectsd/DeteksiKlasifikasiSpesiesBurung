f10d02aded3640714eccc8677a47e507b33cc36a
Kelompok 13 Deep Learning - Sains Data 2024/2025 <deeplearning13project@gmail.com>
2024-12-02 15:14:00 +0700

    Update website.py

--- website.py
+++ website.py
@@ -114,7 +114,7 @@ html_code = """
 """
 
 # Tampilkan form HTML
-st.components.v1.html(html_code, height=350, scrolling=False)
+st.components.v1.html(html_code, height=380, scrolling=False)
 
 # Tangkap file audio yang diunggah
 uploaded_file = st.file_uploader("Upload File Audio Here", type=["mp3"])
