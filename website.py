e908fe8b4d363a2dc58277d9061b663269125e71
Kelompok 13 Deep Learning - Sains Data 2024/2025 <deeplearning13project@gmail.com>
2024-12-08 14:45:47 +0700

    Update website.py

--- website.py
+++ website.py
@@ -3,7 +3,7 @@ from io import BytesIO
 #filename-> nantinya hasil output kelas dari prediksi model(ex:1,2,3...)
 
 # Fungsi untuk mendapatkan URL gambar berdasarkan nama file
-def get_image_url(file_name):
+def get_image_url(file_name):    
     if file_name == "1.mp3":
         return "https://raw.githubusercontent.com/deeplearning13projectsd/DeteksiKlasifikasiSpesiesBurung/main/Deployment/asset/kelas/1.png"
     elif file_name == "2.mp3":
