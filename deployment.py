6923cc3da559341f0cc5eb3fd91eed8b74bffa8c
Kelompok 13 Deep Learning - Sains Data 2024/2025 <deeplearning13project@gmail.com>
2024-12-13 14:48:34 +0700

    Update deployment.py

--- deployment.py
+++ deployment.py
@@ -211,7 +211,7 @@ def add_custom_footer():
             <h5 style="margin: 5px 0;">AVCLA</h5>
             <p style="margin: 5px 0;">AVCLA (Aves Classification), merupakan sebuah website dalam mendeteksi klasifikasi burung di kawasan Taman Nasional Way Kambas. Website ini bisa mendeteksi klasifikasi pada 10 spesies burung, dengan memanfaatkan metode Deep Learning menggunakan Transfer Learning dengan arsitektur Convnext Type Base. Website ini bisa memprediksi spesies burung berdasarkan suara. Model ini dibangun dalam memenuhi Case Based Learning pada matakuliah SD4102Deep Learning di Program Studi Sains Data. Model ini bisa melakukan klasifikasi spesies burung dengan akurasi rata-rata 70%</p>
             <h5 style="margin: 0;">© Developer: Kelompok 13 Deep Learning</h5>
-            <img src="https://raw.githubusercontent.com/alberanalafean22/DeteksiKlasifikasiSpesiesBurung/main/Deployment/asset/Logo0.png" alt="Logo kelompok" width="70" height="70">
+            <img src="https://raw.githubusercontent.com/alberanalafean22/DeteksiKlasifikasiSpesiesBurung/main/Deployment/asset/Logo0.png" alt="Logo kelompok" width="85" height="85">
             <h5 style="margin: 5px 0;">Version 1.0.1 2024</h5>
             <p>
                 <a href="https://github.com/sains-data/Implementasi-Model-Transfer-Learning-Arsitektur-ConvNeXt-untuk-Klasifikasi-Suara-Burung-di-TNWK" target="_blank">
