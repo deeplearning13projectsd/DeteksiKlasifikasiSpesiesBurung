bf2b197328c30be4e6ca4c0387a92180667b2f9a
Kelompok 13 Deep Learning - Sains Data 2024/2025 <deeplearning13project@gmail.com>
2024-12-13 13:45:33 +0700

    Update deployment.py

--- deployment.py
+++ deployment.py
@@ -195,7 +195,7 @@ if uploaded_file:
         image_url = "https://raw.githubusercontent.com/deeplearning13projectsd/DeteksiKlasifikasiSpesiesBurung/main/Deployment/asset/kelas/h.png"
     elif kelas  == 'Spilopelia Chinensis':
         image_url = "https://raw.githubusercontent.com/deeplearning13projectsd/DeteksiKlasifikasiSpesiesBurung/main/Deployment/asset/kelas/i.png"
-    elif kelas  == 'Spilopelia Chinensis':
+    elif kelas  == 'Surniculus Lugubris':
         image_url = "https://raw.githubusercontent.com/deeplearning13projectsd/DeteksiKlasifikasiSpesiesBurung/main/Deployment/asset/kelas/j.png"
 
     st.image(image_url, caption=f"Kelas: {kelas}", use_column_width=False)
@@ -207,9 +207,9 @@ def add_custom_footer():
     <body>
         <div class="footer" style="text-align: center; margin-top: 30px; padding: 10px">
             <h5 style="margin: 5px 0;">AVCLA</h5>
-            <p style="margin: 5px 0;">AVCLA, merupakan sebuah website dalam mendeteksi klasifikasi burung di kawasan Taman Nasional Way Kambas. Website ini bisa mendeteksi klasifikasi pada 10 spesies burung, dengan memanfaatkan deep learning menggunakan transfer learning dengan arsitektur Convnext Type Base. Website ini bisa memprediksi spesies burung berdasarkan suara. Model ini dibangun dalam memenuhi Case Based Learning pada matakuliah SD4102Deep Learning di Program Studi Sains Data, Fakultas Sains, Institut Teknologi Sumatera</p>
+            <p style="margin: 5px 0;">AVCLA, merupakan sebuah website dalam mendeteksi klasifikasi burung di kawasan Taman Nasional Way Kambas. Website ini bisa mendeteksi klasifikasi pada 10 spesies burung, dengan memanfaatkan deep learning menggunakan transfer learning dengan arsitektur Convnext Type Base. Website ini bisa memprediksi spesies burung berdasarkan suara. Model ini dibangun dalam memenuhi Case Based Learning pada matakuliah SD4102Deep Learning di Program Studi Sains Data</p>
+            <h5 style="margin: 0;">© Developer: Kelompok 13 Deep Learning</h5>
             <h5 style="margin: 5px 0;">Version 1.2.1 2024</h5>
-            <h4 style="margin: 0;">© Developer: Kelompok 13 Deep Learning</h4>
       </div>
     </body>
     </html>
