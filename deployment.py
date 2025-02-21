7c49faa2c3cd21a1b504fcd756159c2927d2a707
Kelompok 13 Deep Learning - Sains Data 2024/2025 <deeplearning13project@gmail.com>
2025-02-21 22:54:27 +0700

    Update deployment.py

--- deployment.py
+++ deployment.py
@@ -17,7 +17,7 @@ def add_custom_header():
     <body margin: 20px;">
         <div style="text-align: center; margin-top: 30px; padding: 10px">
             <h1 style="font-size: 35px; tyle="text-align: center; margin-top: 20px;">
-                 <img src="https://raw.githubusercontent.com/deeplearning13projectsd/DeteksiKlasifikasiSpesiesBurung/main/Deployment/asset/avcla.png" alt="Logo kelompok" width="350" height="350">
+                 <img src="https://raw.githubusercontent.com/deeplearning13projectsd/DeteksiKlasifikasiSpesiesBurung/main/Deployment/asset/avcla.png" alt="Logo kelompok" width="250" height="250">
                 AVCLA (Aves Classification)
             </h1>
         </div>
